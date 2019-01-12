0754cebd06487659493c94369dfb93a600e5aefe
Cristian <cristian.pu.go@gmail.com>
2019-01-11 21:00:35 -0500

    Login Facebook con Firebase

--- app/src/main/java/com/example/surreal/shotpingdef/LoginActivity.java
+++ app/src/main/java/com/example/surreal/shotpingdef/LoginActivity.java
@@ -3,19 +3,37 @@ package com.example.surreal.shotpingdef;
 
 import android.content.Intent;
 import android.os.Handler;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.RelativeLayout;
 import android.widget.Toast;
 
+import com.facebook.AccessToken;
 import com.facebook.CallbackManager;
 import com.facebook.FacebookCallback;
 import com.facebook.FacebookException;
 import com.facebook.login.LoginResult;
 import com.facebook.login.widget.LoginButton;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthCredential;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FacebookAuthCredential;
+import com.google.firebase.auth.FacebookAuthProvider;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+import java.util.Arrays;
 
 public class LoginActivity extends AppCompatActivity  {
+
+    private FirebaseAuth auth;
+    private FirebaseUser user;
 
     private LoginButton loginButton;
     private CallbackManager callbackManager;
@@ -42,13 +60,15 @@ public class LoginActivity extends AppCompatActivity  {
 
         handler.postDelayed(runnable, 2000);
 
+        auth = FirebaseAuth.getInstance();
         callbackManager = CallbackManager.Factory.create();
 
         loginButton = (LoginButton)findViewById(R.id.login_button);
+        loginButton.setReadPermissions(Arrays.asList("email"));
         loginButton.registerCallback(callbackManager, new FacebookCallback<LoginResult>() {
             @Override
             public void onSuccess(LoginResult loginResult) {
-                goMainScreen();
+               handleFacebookToken (loginResult.getAccessToken());
             }
 
             @Override
@@ -63,6 +83,24 @@ public class LoginActivity extends AppCompatActivity  {
             }
         });
     }
+
+    private void handleFacebookToken(AccessToken accessToken) {
+        AuthCredential credential = FacebookAuthProvider.getCredential(accessToken.getToken());
+        auth.signInWithCredential(credential)
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        Toast.makeText(LoginActivity.this,""+e.getMessage(), Toast.LENGTH_SHORT).show();
+                        Log.e("ERROR_EDMT",""+e.getMessage());
+                    }
+                }).addOnSuccessListener(new OnSuccessListener<AuthResult>() {
+            @Override
+            public void onSuccess(AuthResult authResult) {
+                goMainScreen();
+            }
+        });
+    }
+
 
     private void goMainScreen() {
 
