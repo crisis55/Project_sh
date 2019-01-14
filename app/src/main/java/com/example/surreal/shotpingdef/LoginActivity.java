cfb70422607b051c115f34995bb819e2b420394b
Cristian <cristian.pu.go@gmail.com>
2019-01-14 13:33:58 -0500

    Añadido formulario de registro y vista mejorada

--- app/src/main/java/com/example/surreal/shotpingdef/LoginActivity.java
+++ app/src/main/java/com/example/surreal/shotpingdef/LoginActivity.java
@@ -8,6 +8,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.Button;
 import android.widget.ProgressBar;
 import android.widget.RelativeLayout;
 import android.widget.Toast;
@@ -36,6 +37,7 @@ public class LoginActivity extends AppCompatActivity  {
     private FirebaseAuth auth;
     private FirebaseUser user;
     private ProgressBar progressBar;
+    private Button registerButton;
 
     private LoginButton loginButton;
     private CallbackManager callbackManager;
@@ -65,6 +67,13 @@ public class LoginActivity extends AppCompatActivity  {
         auth = FirebaseAuth.getInstance();
         progressBar = (ProgressBar) findViewById(R.id.login_progress);
         callbackManager = CallbackManager.Factory.create();
+        registerButton = (Button)findViewById(R.id.register_button_main);
+        registerButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                goRegisterScreen();
+            }
+        });
 
         loginButton = (LoginButton)findViewById(R.id.login_button);
         loginButton.setReadPermissions(Arrays.asList("email"));
@@ -113,6 +122,12 @@ public class LoginActivity extends AppCompatActivity  {
 
         Intent intent = new Intent(this, MainActivity.class);
         intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP | Intent.FLAG_ACTIVITY_CLEAR_TASK | Intent.FLAG_ACTIVITY_NEW_TASK);
+        startActivity(intent);
+    }
+
+    private void goRegisterScreen () {
+
+        Intent intent = new Intent(this, RegisterActivity.class);
         startActivity(intent);
     }
 
