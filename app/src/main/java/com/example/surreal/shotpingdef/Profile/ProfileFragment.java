2b869bddab776ffe74c3b5def15fc67f44be01f4
Cristian <cristian.pu.go@gmail.com>
2019-02-01 11:00:01 -0500

    Toolbar y menu del perfil añadidos

--- app/src/main/java/com/example/surreal/shotpingdef/Profile/ProfileFragment.java
+++ app/src/main/java/com/example/surreal/shotpingdef/Profile/ProfileFragment.java
@@ -4,7 +4,12 @@ import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 
@@ -14,6 +19,29 @@ public class ProfileFragment extends Fragment {
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate (R.layout.fragment_profile, container, false);
+        View view = inflater.inflate (R.layout.fragment_profile, container, false);
+
+
+            Toolbar toolbar = (Toolbar) view.findViewById(R.id.profileToolbar);
+            ((AppCompatActivity)getActivity()).setSupportActionBar(toolbar);
+            setHasOptionsMenu(true);
+            toolbar.setOnMenuItemClickListener(new Toolbar.OnMenuItemClickListener() {
+                @Override
+                public boolean onMenuItemClick(MenuItem menuItem) {
+
+                    switch (menuItem.getItemId()){
+                       case R.id.profileMenu:
+                    }
+                    return false;
+                }
+            });
+
+        return view;
+    }
+
+    @Override
+    public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
+        inflater.inflate(R.menu.profile_menu, menu);
+        super.onCreateOptionsMenu(menu,inflater);
     }
 }
